748b5414410fb04d6bb44ad35f2da09837509ffa
DN-debug <75267055+DN-debug@users.noreply.github.com>
2022-01-04 12:17:01 +0530

    commit - install.sh
    this wont really install anything, would only put the files into /tmp directory

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+##################################################
+## Author - DN-debug
+## Description - Peux RiverWM setup via fetcher app
+##################################################
+
+# No longer prompting users to install components or dependencies.
+# Downloaded files would remain in /tmp/ directory for user to
+# fetch it all by themselves.
+notify-send "Files are present in '/tmp/Peux-River'."
