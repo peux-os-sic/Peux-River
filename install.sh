d57f718fdeeae6bdf5f9005dcc14d262a5bc51d2
DN-debug <75267055+DN-debug@users.noreply.github.com>
2022-01-04 12:20:13 +0530

    commit

--- install.sh
+++ install.sh
@@ -6,7 +6,21 @@
 ## Description - Peux RiverWM setup via fetcher app
 ##################################################
 
-# No longer prompting users to install components or dependencies.
-# Downloaded files would remain in /tmp/ directory for user to
-# fetch it all by themselves.
-notify-send "Files are present in '/tmp/fetcher/Peux-River'."
+# copy configurations
+
+if zenity --question --text="Want to copy the configurations?"
+then
+    echo "changing directory"
+    cd ../
+    mkdir tmp
+    cd tmp/
+    cp -r Peux-River/*/ $HOME/.config/
+    notify-send "Copied the configurations to $HOME/tmp/"
+else
+    notify-send "skipping the setup!"
+fi
+
+# cleanup
+
+rm -rf /tmp/fetcher/Peux-River
+notify-send "Done!"
